c543a20a2ced7aca8ab7ba89c640ec3b549f22af
KellyTran2024 <ketran4@s.sfusd.edu>
2025-04-27 22:42:56 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -10,8 +10,12 @@ public class LightBoard
    */
   public LightBoard(int numRows, int numCols)
   {
-    /* to be implemented in part (a) */
-
+    lights = new boolean[numRows][numCols];
+    for (int r = 0; r < numRows; r++) {
+        for (int c = 0; c < numCols; c++) {
+            lights[r][c] = Math.random() < 0.4;
+        }
+    }
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
@@ -20,9 +24,23 @@ public class LightBoard
    */
   public boolean evaluateLight(int row, int col)
   {
-    /* to be implemented in part (b) */
+    int count = 0;
+    for (int r = 0; r < lights.length; r++) {
+        if (lights[r][col]) {
+            count++;
+    }
+    }
+    if (lights[row][col]) { 
+        if (count % 2 == 0) {
+            return false;
+        }
+    } else { 
+if (count % 3 == 0) {
+            return true;
+ }
+    }
+    return lights[row][col];
    
- 
   }
   public boolean[][] getLights()
   {
